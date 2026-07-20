af3389a74cc4a0cf901064bc9b507035d62058ec
difan1995 <140031348+difan1995@users.noreply.github.com>
2026-07-20 15:20:22 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+from waggle.plugin import Plugin
+from waggle.data.vision import Camera
+
+
+def compute_mean_color(image):
+    return np.mean(image, (0, 1)).astype(float)
+
+
+def main():
+    with Plugin() as plugin, Camera("file:///Users/difan/Desktop/UFL/2026summer/di2/example.jpg") as camera:
+        # read example image from file
+        image = camera.snapshot()
+
+        # compute mean color
+        mean_color = compute_mean_color(image.data)
+
+        # print mean color
+        print(mean_color)
+        plugin.publish("color.mean.r", mean_color[0], timestamp=image.timestamp)
+        plugin.publish("color.mean.g", mean_color[1], timestamp=image.timestamp)
+        plugin.publish("color.mean.b", mean_color[2], timestamp=image.timestamp)
+        image.save("snapshot.jpg")
+        plugin.upload_file("snapshot.jpg", timestamp=image.timestamp)
+
+if __name__ == "__main__":
+    main()
